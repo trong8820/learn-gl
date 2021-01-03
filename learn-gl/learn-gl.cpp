657517c0d519d68c61025c83179cffd8a9eeb8a9
Cao Trong <trong8820@gmail.com>
2021-01-03 17:19:31 +0700

    Create project

---
+++ learn-gl/learn-gl.cpp
@@ -0,0 +1,12 @@
+﻿// learn-gl.cpp : Defines the entry point for the application.
+//
+
+#include "learn-gl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
