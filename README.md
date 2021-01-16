a4e3324e9796772d9b714faeb1d7cfa757c84970
Cao Trong <trong8820@gmail.com>
2021-01-17 02:14:06 +0700

    Show window

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learn-gl
+
+3rdparty: drive/Projects/redbean/3rdparty.zip</br>
+delete .DS_Store files on windows: cmd: del /s /q /f /a .DS_STORE_
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+auto main() -> int
+{
+	if (glfwInit() == GLFW_FALSE) return EXIT_FAILURE;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#else
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+#endif
+
+#if defined(_DEBUG) || defined(DEBUG) || !defined(NDEBUG)
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+	glfwSetErrorCallback([](int error, const char* description)
+	{
+		std::cout << "GLFW Error: " << error << " - "<< description << "\n";
+	});
+#endif
+
+	auto pWindow = glfwCreateWindow(800, 600, "Example 01", nullptr, nullptr);
+	if (pWindow == nullptr) return EXIT_FAILURE;
+
+	glfwMakeContextCurrent(pWindow);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return EXIT_FAILURE;
+
+	int width{ 0 };
+	int height{ 0 };
+	glfwGetWindowSize(pWindow, &width, &height);
+	//float xscale{ 1.0f };
+	//float yscale{ 1.0f };
+	//glfwGetWindowContentScale(m_pWindow, &xscale, &yscale);
+
+	glfwSwapInterval(1);
+	while (glfwWindowShouldClose(pWindow) == GLFW_FALSE)
+	{
+		glfwSwapBuffers(pWindow);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(pWindow);
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
