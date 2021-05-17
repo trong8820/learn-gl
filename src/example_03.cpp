6abd15eeb845f36ff9b2930d3da617652bb8b717
Cao Trong <trong8820@gmail.com>
2021-05-17 18:28:50 +0700

    Update

--- src/example_03.cpp
+++ src/example_03.cpp
@@ -9,6 +9,11 @@
 
 const float PI = 3.14159265358979f;
 
+const int table[16 * 6] =
+{
+
+};
+
 float positions[9];
 
 const char *scalarVertexShaderSource = R"(
@@ -46,20 +51,76 @@ void main()
 const char *cellVertexShaderSource = R"(
 #version 410 core
 
+uniform int samples;
 uniform sampler2D scalar;
+
+flat out int outCell;
 
 void main()
 {
-    int x = gl_VertexID / 64;
-    int y = gl_VertexID % 64;
-    vec2 pos = vec2(x, y) / (64 - 1.0);
-    float s = textureLod(scalar, pos, 0.0).r;
-    gl_Position = vec4(pos - vec2(0.5, 0.5), 0.0f, 1.0);
-    gl_PointSize = s > 12.0 ? 2.0 : 1.0;
+    int x = gl_VertexID / samples;
+    int y = gl_VertexID % samples;
+
+    outCell = 0;
+
+    vec2 pos0 = vec2(x + 0, y + 0) / float(samples);
+    float s0 = textureLod(scalar, pos0, 0.0).r;
+    if (s0 < 12.0)
+    {
+        outCell |= (1 << 0);
+    }
+
+    vec2 pos1 = vec2(x + 0, y + 1) / float(samples);
+    float s1 = textureLod(scalar, pos1, 0.0).r;
+    if (s1 < 12.0)
+    {
+        outCell |= (1 << 1);
+    }
+
+    vec2 pos2 = vec2(x + 1, y + 1) / float(samples);
+    float s2 = textureLod(scalar, pos2, 0.0).r;
+    if (s2 < 12.0)
+    {
+        outCell |= (1 << 2);
+    }
+    
+    vec2 pos3 = vec2(x + 1, y + 0) / float(samples);
+    float s3 = textureLod(scalar, pos3, 0.0).r;
+    if (s3 < 12.0)
+    {
+        outCell |= (1 << 3);
+    }
 }
 )";
 
 const char *cellFragmentShaderSource = R"(
+#version 410 core
+
+void main()
+{
+}
+)";
+
+const char *marchingVertexShaderSource = R"(
+#version 410 core
+
+uniform int samples;
+uniform isampler2D cell;
+uniform isampler2D table;
+
+void main()
+{
+    int x = gl_VertexID / samples;
+    int y = gl_VertexID % samples;
+
+    vec2 pos = vec2(x, y) / float(samples);
+    int s = textureLod(cell, pos, 0.0).r;
+    gl_Position = vec4(pos - vec2(0.5, 0.5), 0.0f, 1.0);
+    gl_PointSize = s == 0 ? 4.0 : 2.0;
+}
+)";
+
+const char *marchingFragmentShaderSource = R"(
 #version 410 core
 
 out vec4 FragColor;
@@ -79,6 +140,10 @@ GLuint gScalarTFBO;
 GLint gPosLoc;
 
 GLuint gCellProgram;
+GLuint gCellTFO;
+GLuint gCellTFBO;
+
+GLuint gMarchingProgram;
 
 float gTime = 0.0f;
 
@@ -94,7 +159,6 @@ auto init() -> bool
         GL_CHECK(glCompileShader(vertexShader));
         {
             GLint success;
-            GLchar infoLog[1024];
             GL_CHECK(glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success));
             if(!success)
             {
@@ -113,7 +177,6 @@ auto init() -> bool
         GL_CHECK(glCompileShader(fragmentShader));
         {
             GLint success;
-            GLchar infoLog[1024];
             GL_CHECK(glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success));
             if(!success)
             {
@@ -144,7 +207,6 @@ auto init() -> bool
         GL_CHECK(glCompileShader(vertexShader));
         {
             GLint success;
-            GLchar infoLog[1024];
             GL_CHECK(glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success));
             if(!success)
             {
@@ -163,7 +225,6 @@ auto init() -> bool
         GL_CHECK(glCompileShader(fragmentShader));
         {
             GLint success;
-            GLchar infoLog[1024];
             GL_CHECK(glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success));
             if(!success)
             {
@@ -180,9 +241,57 @@ auto init() -> bool
         gCellProgram = GL_CHECK_RETURN(glCreateProgram());
         GL_CHECK(glAttachShader(gCellProgram, vertexShader));
         GL_CHECK(glAttachShader(gCellProgram, fragmentShader));
+        const GLchar* feedbackVaryings[] = { "outCell" };
+        glTransformFeedbackVaryings(gCellProgram, 1, feedbackVaryings, GL_SEPARATE_ATTRIBS);
+        GL_CHECK(glLinkProgram(gCellProgram));
+
+        GL_CHECK(glDeleteShader(vertexShader));
+        GL_CHECK(glDeleteShader(fragmentShader));
+    }
+
+    {
+        auto vertexShader = GL_CHECK_RETURN(glCreateShader(GL_VERTEX_SHADER));
+        GL_CHECK(glShaderSource(vertexShader, 1, &marchingVertexShaderSource, NULL));
+        GL_CHECK(glCompileShader(vertexShader));
+        {
+            GLint success;
+            GL_CHECK(glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success));
+            if(!success)
+            {
+                GLint infoLength = 0;
+                glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &infoLength);
+                char* infoLog = new char[infoLength];
+                GL_CHECK(glGetShaderInfoLog(vertexShader, infoLength, &infoLength, infoLog));
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << "VERTEX_SHADER" << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+                delete [] infoLog;
+                GL_CHECK(glDeleteShader(vertexShader));
+            }
+        }
+
+        auto fragmentShader = GL_CHECK_RETURN(glCreateShader(GL_FRAGMENT_SHADER));
+        GL_CHECK(glShaderSource(fragmentShader, 1, &marchingFragmentShaderSource, NULL));
+        GL_CHECK(glCompileShader(fragmentShader));
+        {
+            GLint success;
+            GL_CHECK(glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success));
+            if(!success)
+            {
+                GLint infoLength = 0;
+                glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &infoLength);
+                char* infoLog = new char[infoLength];
+                GL_CHECK(glGetShaderInfoLog(fragmentShader, infoLength, &infoLength, infoLog));
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << "FRAGMENT_SHADER" << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+                delete [] infoLog;
+                GL_CHECK(glDeleteShader(fragmentShader));
+            }
+        }
+
+        gMarchingProgram = GL_CHECK_RETURN(glCreateProgram());
+        GL_CHECK(glAttachShader(gMarchingProgram, vertexShader));
+        GL_CHECK(glAttachShader(gMarchingProgram, fragmentShader));
         //const GLchar* feedbackVaryings[] = { "outCell" };
         //glTransformFeedbackVaryings(gCellProgram, 1, feedbackVaryings, GL_SEPARATE_ATTRIBS);
-        GL_CHECK(glLinkProgram(gCellProgram));
+        GL_CHECK(glLinkProgram(gMarchingProgram));
 
         GL_CHECK(glDeleteShader(vertexShader));
         GL_CHECK(glDeleteShader(fragmentShader));
@@ -215,6 +324,33 @@ auto init() -> bool
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    glUseProgram(gCellProgram);
+    glUniform1i(glGetUniformLocation(gCellProgram, "samples"), SAMPLES - 1);
+    glUniform1i(glGetUniformLocation(gCellProgram, "scalar"), 0);
+
+    glGenTransformFeedbacks(1, &gCellTFO);
+    glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, gCellTFO);
+
+    glGenBuffers(1, &gCellTFBO);
+    glBindBuffer(GL_TRANSFORM_FEEDBACK_BUFFER, gCellTFBO);
+    glBufferData(GL_TRANSFORM_FEEDBACK_BUFFER,  (SAMPLES - 1) * (SAMPLES - 1) * sizeof(GLint), NULL, GL_STATIC_DRAW);
+    glBindTransformFeedback(GL_TRANSFORM_FEEDBACK_BUFFER, gCellTFBO);
+    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, gCellTFBO);
+
+    GLuint cellTexture;
+    glGenTextures(1, &cellTexture);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, cellTexture);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_R32I, SAMPLES - 1, SAMPLES - 1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    glUseProgram(gMarchingProgram);
+    glUniform1i(glGetUniformLocation(gMarchingProgram, "samples"), SAMPLES - 1);
+    glUniform1i(glGetUniformLocation(gMarchingProgram, "cell"), 1);
 
 	size();
 
@@ -268,8 +404,22 @@ auto draw() -> void
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, gScalarTFBO);
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, SAMPLES, SAMPLES, GL_RED, GL_FLOAT, NULL);
 
+    glEnable(GL_RASTERIZER_DISCARD);
     glUseProgram(gCellProgram);
-    glDrawArrays(GL_POINTS, 0, SAMPLES * SAMPLES);
+
+    glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, gCellTFO);
+        glBeginTransformFeedback(GL_POINTS);
+	        glDrawArrays(GL_POINTS, 0, (SAMPLES - 1) * (SAMPLES - 1));
+        glEndTransformFeedback();
+    glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, 0);
+    glDisable(GL_RASTERIZER_DISCARD);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, gCellTFBO);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, SAMPLES - 1, SAMPLES - 1, GL_RED_INTEGER, GL_INT, NULL);
+
+    glUseProgram(gMarchingProgram);
+    glDrawArrays(GL_POINTS, 0, (SAMPLES - 1) * (SAMPLES - 1));
 }
 
 auto main() -> int
