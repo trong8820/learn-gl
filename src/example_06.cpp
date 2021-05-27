3270710ba42fe8ce87e468fa82c5b3492d18a31f
Cao Trong <trong8820@gmail.com>
2021-05-27 16:27:24 +0700

    example 06

--- src/example_06.cpp
+++ src/example_06.cpp
@@ -37,28 +37,6 @@ unsigned int indices[] =
 	1, 7, 5
 };
 
-const char* shadownVertexShaderSource = R"(
-#version 410 core
-
-layout (location = 0) in vec3 aPos;
-
-uniform mat4 lightSpace;
-uniform mat4 world;
-
-void main()
-{
-	gl_Position = lightSpace * world * vec4(aPos, 1.0);
-}
-)";
-
-const char* shadownFragmentShaderSource = R"(
-#version 410 core
-
-void main()
-{
-}
-)";
-
 const char *vertexShaderSource = R"(
 #version 410 core
 
@@ -68,15 +46,12 @@ layout (location = 1) in vec3 aColor;
 uniform mat4 world;
 uniform mat4 view;
 uniform mat4 proj;
-uniform mat4 lightSpace;
 
-out vec4 vPosLightSpace;
 out vec3 vColor;
 
 void main()
 {
 	vColor = aColor;
-	vPosLightSpace = lightSpace * world * vec4(aPos, 1.0);
 	gl_Position = proj * view * world * vec4(aPos, 1.0);
 }
 )";
@@ -84,56 +59,154 @@ void main()
 const char *fragmentShaderSource = R"(
 #version 410 core
 
-uniform sampler2D shadowMap;
-
-in vec4 vPosLightSpace;
 in vec3 vColor;
 
 out vec4 FragColor;
 
 void main()
 {
-	vec3 projCoords = vPosLightSpace.xyz / vPosLightSpace.w;
-	projCoords = projCoords * 0.5 + 0.5;
-	float closestDepth = texture(shadowMap, projCoords.xy).r; 
-	float shadow = projCoords.z - 0.0001 > closestDepth ? 0.3 : 1.0;
-	FragColor = vec4(vColor*shadow, 1.0);
+	FragColor = vec4(vColor, 1.0);
 }
 )";
 
-GLuint gShadowProgram;
-GLint gShadowWorldLoc;
+const char *silhouetteVertexShaderSource = R"(
+#version 410 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+uniform mat4 world;
+uniform mat4 view;
+uniform mat4 proj;
+
+out vec3 vPos;
+
+void main()
+{
+	gl_Position = proj * view * world * vec4(aPos, 1.0);
+	vPos = (world * vec4(aPos, 1.0)).xyz;
+}
+)";
+
+const char *silhouetteGeometryShaderSource = R"(
+#version 410 core
+
+layout (triangles_adjacency) in;
+layout (line_strip, max_vertices = 6) out;
+
+//uniform vec3 lightPos;
+
+in vec3 vPos[];
+
+void EmitLine(int StartIndex, int EndIndex)
+{
+    gl_Position = gl_in[StartIndex].gl_Position;
+    EmitVertex();
+
+    gl_Position = gl_in[EndIndex].gl_Position;
+    EmitVertex();
+
+    EndPrimitive();
+}
+
+void main()
+{
+	vec3 e1 = vPos[2] - vPos[0];
+    vec3 e2 = vPos[4] - vPos[0];
+    vec3 e3 = vPos[1] - vPos[0];
+    vec3 e4 = vPos[3] - vPos[2];
+    vec3 e5 = vPos[4] - vPos[2];
+    vec3 e6 = vPos[5] - vPos[0];
+
+	vec3 lightPos = vec3(-3.0, 5.0, -1.0);
+	vec3 faceNormal = cross(e1, e2);
+    vec3 lightDir = lightPos - vPos[0];
+	if (dot(faceNormal, lightDir) > 0.00001) 
+	{
+		faceNormal = cross(e3, e1);
+		if (dot(faceNormal, lightDir) <= 0.0) 
+		{
+			EmitLine(0, 2);
+		}
+
+		faceNormal = cross(e4, e5);
+		lightDir = lightPos - vPos[2];
+		if (dot(faceNormal, lightDir) <= 0.0) 
+		{
+			EmitLine(2, 4);
+		}
+
+		faceNormal = cross(e2, e6);
+		lightDir = lightPos - vPos[4];
+		if (dot(faceNormal, lightDir) <= 0.0) 
+		{
+			EmitLine(4, 0);
+		}
+	}
+}
+)";
+
+const char *silhouetteFragmentShaderSource = R"(
+#version 410 core
+
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+)";
 
 GLuint gProgram;
+GLuint gSilhouetteProgram;
+
 GLuint gVAO;
 
 GLint gWorldLoc;
+GLint gSilhouetteWorldLoc;
 float gAngle = 0.0f;
-
-GLuint gDepthFBO;
-GLuint gDepthMap;
 
 auto init() -> bool
 {
-	//std::cout << "init " << gWidth << " " << gHeight << std::endl;
+	unsigned int indices_adj[72];
+	size_t id = 0;
+	for (size_t i = 0; i < 12; i++)
 	{
-		auto vertexShader = GL_CHECK_RETURN(glCreateShader(GL_VERTEX_SHADER));
-		GL_CHECK(glShaderSource(vertexShader, 1, &shadownVertexShaderSource, NULL));
-		GL_CHECK(glCompileShader(vertexShader));
+		for (size_t j = 0; j < 3; j++)
+		{
+			unsigned int id0 = indices[i*3 + j];
+			unsigned int id1 = indices[i*3 + (j + 1)%3];
 
-		auto fragmentShader = GL_CHECK_RETURN(glCreateShader(GL_FRAGMENT_SHADER));
-		GL_CHECK(glShaderSource(fragmentShader, 1, &shadownFragmentShaderSource, NULL));
-		GL_CHECK(glCompileShader(fragmentShader));
+			indices_adj[id++] = id0;
+			for (size_t k = 0; k < 12; k++)
+			{
+				if (k == i) continue;
+				unsigned int k0 = indices[k*3 + 0];
+				unsigned int k1 = indices[k*3 + 1];
+				unsigned int k2 = indices[k*3 + 2];
+				
+				if ((k0 == id0 && k1 == id1) || (k1 == id0 && k0 == id1))
+				{
+					indices_adj[id++] = k2;
+					break;
+				}
 
-		gShadowProgram = GL_CHECK_RETURN(glCreateProgram());
-		GL_CHECK(glAttachShader(gShadowProgram, vertexShader));
-		GL_CHECK(glAttachShader(gShadowProgram, fragmentShader));
-		GL_CHECK(glLinkProgram(gShadowProgram));
+				if ((k1 == id0 && k2 == id1) || (k2 == id0 && k1 == id1))
+				{
+					indices_adj[id++] = k0;
+					break;
+				}
 
-		GL_CHECK(glDeleteShader(vertexShader));
-		GL_CHECK(glDeleteShader(fragmentShader));
+				if ((k2 == id0 && k0 == id1) || (k0 == id0 && k2 == id1))
+				{
+					indices_adj[id++] = k1;
+					break;
+				}
+			}
+		}
 	}
 
+	//std::cout << "init " << gWidth << " " << gHeight << std::endl;
 	{
 		auto vertexShader = GL_CHECK_RETURN(glCreateShader(GL_VERTEX_SHADER));
 		GL_CHECK(glShaderSource(vertexShader, 1, &vertexShaderSource, NULL));
@@ -147,6 +220,29 @@ auto init() -> bool
 		GL_CHECK(glAttachShader(gProgram, vertexShader));
 		GL_CHECK(glAttachShader(gProgram, fragmentShader));
 		GL_CHECK(glLinkProgram(gProgram));
+
+		GL_CHECK(glDeleteShader(vertexShader));
+		GL_CHECK(glDeleteShader(fragmentShader));
+	}
+
+	{
+		auto vertexShader = GL_CHECK_RETURN(glCreateShader(GL_VERTEX_SHADER));
+		GL_CHECK(glShaderSource(vertexShader, 1, &silhouetteVertexShaderSource, NULL));
+		GL_CHECK(glCompileShader(vertexShader));
+
+		auto geometryShader = GL_CHECK_RETURN(glCreateShader(GL_GEOMETRY_SHADER));
+		GL_CHECK(glShaderSource(geometryShader, 1, &silhouetteGeometryShaderSource, NULL));
+		GL_CHECK(glCompileShader(geometryShader));
+
+		auto fragmentShader = GL_CHECK_RETURN(glCreateShader(GL_FRAGMENT_SHADER));
+		GL_CHECK(glShaderSource(fragmentShader, 1, &silhouetteFragmentShaderSource, NULL));
+		GL_CHECK(glCompileShader(fragmentShader));
+
+		gSilhouetteProgram = GL_CHECK_RETURN(glCreateProgram());
+		GL_CHECK(glAttachShader(gSilhouetteProgram, vertexShader));
+		GL_CHECK(glAttachShader(gSilhouetteProgram, geometryShader));
+		GL_CHECK(glAttachShader(gSilhouetteProgram, fragmentShader));
+		GL_CHECK(glLinkProgram(gSilhouetteProgram));
 
 		GL_CHECK(glDeleteShader(vertexShader));
 		GL_CHECK(glDeleteShader(fragmentShader));
@@ -167,35 +263,13 @@ auto init() -> bool
 		GLuint EBO;
 		glGenBuffers(1, &EBO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glGenTextures(1, &gDepthMap);
-	glBindTexture(GL_TEXTURE_2D, gDepthMap);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, 1024, 1024, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glGenFramebuffers(1, &gDepthFBO);
-	glBindFramebuffer(GL_FRAMEBUFFER, gDepthFBO);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, gDepthMap, 0);
-	glDrawBuffer(GL_NONE);
-	glReadBuffer(GL_NONE);
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);  
-
-	glUseProgram(gShadowProgram);
-	GLint shadownLightSpaceLoc = glGetUniformLocation(gShadowProgram, "lightSpace");
-	gShadowWorldLoc = glGetUniformLocation(gShadowProgram, "world");
-	mat4 view = mat4::lookAt(vec3(-3.0f, 5.0, -1.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
-	mat4 proj = mat4::ortho(8.0f, 8.0f, 0.1f, 100.0f);
-	mat4 lightSpace = view*proj;
-	glUniformMatrix4fv(shadownLightSpaceLoc, 1, false, lightSpace.m);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_adj), indices_adj, GL_STATIC_DRAW);
 
 	glUseProgram(gProgram);
-	GLint lightSpaceLoc = glGetUniformLocation(gProgram, "lightSpace");
 	gWorldLoc = glGetUniformLocation(gProgram, "world");
-	glUniformMatrix4fv(lightSpaceLoc, 1, false, lightSpace.m);
+
+	glUseProgram(gSilhouetteProgram);
+	gSilhouetteWorldLoc = glGetUniformLocation(gSilhouetteProgram, "world");
 
 	on_size();
 
@@ -207,16 +281,25 @@ void on_size()
 	//std::cout << "size " << gWidth << " " << gHeight << std::endl;
 	glViewport(0, 0, gWidth, gHeight);
 
-	glUseProgram(gProgram);
-	gWorldLoc = glGetUniformLocation(gProgram, "world");
-	GLint viewLoc = glGetUniformLocation(gProgram, "view");
-	GLint projLoc = glGetUniformLocation(gProgram, "proj");
-
 	mat4 world = mat4::identity;
 	mat4 view = mat4::lookAt(vec3(0.0f, 3.0, 3.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
 	mat4 proj = mat4::perspective(45.0f * (PI/180.0f), static_cast<float>(gWidth)/gHeight, 0.1f, 100.0f);
-	glUniformMatrix4fv(viewLoc, 1, false, view.m);
-	glUniformMatrix4fv(projLoc, 1, false, proj.m);
+
+	{
+		glUseProgram(gProgram);
+		GLint viewLoc = glGetUniformLocation(gProgram, "view");
+		GLint projLoc = glGetUniformLocation(gProgram, "proj");
+		glUniformMatrix4fv(viewLoc, 1, false, view.m);
+		glUniformMatrix4fv(projLoc, 1, false, proj.m);
+	}
+
+	{
+		glUseProgram(gSilhouetteProgram);
+		GLint viewLoc = glGetUniformLocation(gSilhouetteProgram, "view");
+		GLint projLoc = glGetUniformLocation(gSilhouetteProgram, "proj");
+		glUniformMatrix4fv(viewLoc, 1, false, view.m);
+		glUniformMatrix4fv(projLoc, 1, false, proj.m);
+	}
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glEnable(GL_MULTISAMPLE);
@@ -238,21 +321,6 @@ auto draw() -> void
 	mat4 world1 = mat4::rotate(0.0f, 1.0f, 0.0f, gAngle * (PI/180.0f));
 	mat4 world2 = mat4::scale(6.0f, 0.1f, 6.0f) * mat4::translate(0.0f, -0.5f, 0.0f);
 
-	// 1. first render to depth map
-	glViewport(0, 0, 1024, 1024);
-	glBindFramebuffer(GL_FRAMEBUFFER, gDepthFBO);
-		glClear(GL_DEPTH_BUFFER_BIT);
-		glUseProgram(gShadowProgram);
-		glBindVertexArray(gVAO);
-		
-		glUniformMatrix4fv(gWorldLoc, 1, false, world1.m);
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-
-		glUniformMatrix4fv(gWorldLoc, 1, false, world2.m);
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
 	glViewport(0, 0, gWidth, gHeight);
 	glClearColor(0.0f, 0.2f, 0.2f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -260,14 +328,17 @@ auto draw() -> void
 	glUseProgram(gProgram);
 	glBindVertexArray(gVAO);
 
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, gDepthMap);
-
 	glUniformMatrix4fv(gWorldLoc, 1, false, world1.m);
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES_ADJACENCY, 72, GL_UNSIGNED_INT, 0);
+	
 
-	glUniformMatrix4fv(gWorldLoc, 1, false, world2.m);
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glDepthFunc(GL_ALWAYS);
+	glUseProgram(gSilhouetteProgram);
+	glBindVertexArray(gVAO);
+
+	glUniformMatrix4fv(gSilhouetteWorldLoc, 1, false, world1.m);
+	glDrawElements(GL_TRIANGLES_ADJACENCY, 72, GL_UNSIGNED_INT, 0);
+	glDepthFunc(GL_LESS);  
 }
 
 auto main() -> int
